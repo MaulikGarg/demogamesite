fb8566e73e259724731e0c1b3ae0c855e411d060
Maulik Garg <maulikgarg678@gmail.com>
2026-06-18 19:22:19 +0530

    add audio and fine tune values

--- src/script.js
+++ src/script.js
@@ -4,10 +4,14 @@ const links = document.querySelectorAll('a');
 // the element itself
 const numberDisplay = document.querySelector('#hover-button-number');
 
+const hoverSound = new Audio('assets/menu1.wav');
+
 // attach the text changing event listener on all of em
 links.forEach(button => {
     button.addEventListener('mouseenter', function () {
         numberDisplay.textContent = button.dataset.number;
+        hoverSound.currentTime = 0;
+        hoverSound.play();
     });
     button.addEventListener('mouseleave', function () {
         numberDisplay.textContent = "67";
