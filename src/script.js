d3585501a746fa34098d90f0297a090717097ff4
Maulik Garg <maulikgarg678@gmail.com>
2026-06-18 17:37:06 +0530

    firscommit

---
+++ src/script.js
@@ -0,0 +1,15 @@
+// ok first select all buttons
+const links = document.querySelectorAll('a');
+
+// the element itself
+const numberDisplay = document.querySelector('#hover-button-number');
+
+// attach the text changing event listener on all of em
+links.forEach(button => {
+    button.addEventListener('mouseenter', function () {
+        numberDisplay.textContent = button.dataset.number;
+    });
+    button.addEventListener('mouseleave', function () {
+        numberDisplay.textContent = "67";
+    });
+});
